7e30a6b8c2397ede1bf37ac69812f24f2e523fb0
Tran Tan Tai <65342718+Kuerl@users.noreply.github.com>
2020-09-18 11:35:38 +0700

    First Class

---
+++ ClassandObject/First.java
@@ -0,0 +1,26 @@
+package ClassandObject;
+
+import java.util.Scanner;
+
+class Student{
+	Scanner sc = new Scanner(System.in);
+	String name;
+	int age;
+
+	public void display() {
+		System.out.println("Name: "+name+"\nAge: "+age);
+	}
+	public void getInformation() {
+		name = sc.next();
+		age = sc.nextInt();
+	}
+}
+
+
+public class First {
+	public static void main(String[]args) {
+		Student s1 = new Student();
+		s1.getInformation();
+		s1.display();
+	}
+}
